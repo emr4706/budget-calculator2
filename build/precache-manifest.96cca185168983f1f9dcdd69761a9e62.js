bdd0b78cf6ff8e2e695fbb0df072435c80c25547
emr4706 <emr4706@gmail.com>
2020-01-19 22:13:15 +0100

    update

---
+++ build/precache-manifest.96cca185168983f1f9dcdd69761a9e62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac06d5800af99dc65b2df18cfafd86be",
+    "url": "/budget-calculator2/index.html"
+  },
+  {
+    "revision": "659f92610446fe8b2619",
+    "url": "/budget-calculator2/static/css/main.dfddcf41.chunk.css"
+  },
+  {
+    "revision": "76604d4f94710a0535a5",
+    "url": "/budget-calculator2/static/js/2.47e3d584.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/budget-calculator2/static/js/2.47e3d584.chunk.js.LICENSE"
+  },
+  {
+    "revision": "659f92610446fe8b2619",
+    "url": "/budget-calculator2/static/js/main.76a15239.chunk.js"
+  },
+  {
+    "revision": "6f91b72e34f29ccd8246",
+    "url": "/budget-calculator2/static/js/runtime-main.a9d36fcf.js"
+  }
+]);
